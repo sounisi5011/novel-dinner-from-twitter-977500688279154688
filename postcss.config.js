6615821fa0ce9d200be4678f86e5d7c7b6b5f09b
Sonishi Izuka <sounisi5011@jedofuka.local>
2018-04-05 05:53:22 +0900

    Edit postcss.config.js file: Add autoprefixer plugin

--- postcss.config.js
+++ postcss.config.js
@@ -2,6 +2,9 @@ module.exports = {
   map: { inline: false },
   plugins: [
     require('postcss-import'),
+    require('autoprefixer')({
+      remove: false,
+    }),
     require('postcss-clean')({
       level: {
         1: {
