3a7eab34a61f753cbec592864a3594eebd1246fc
Sonishi Izuka <sounisi5011@jedofuka.local>
2018-04-05 09:05:53 +0900

    Edit postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,46 @@
-module.exports = {
-  map: { inline: false },
-  plugins: [
-    require('postcss-import'),
-    require('autoprefixer')({
-      remove: false,
-    }),
-    require('postcss-clean')({
-      level: {
-        1: {
-          optimizeFontWeight: false,
-        },
-        2: {
-          all: false,
-        },
-      },
-    }),
-  ],
+const path = require('path');
+
+module.exports = ctx => {
+  const fixStylePath = path.join(ctx.cwd, 'src/css/browser-fix-style');
+
+  if (ctx.file.dirname.startsWith(fixStylePath)) {
+    /*
+     * browser-fix-styleディレクトリ以下のCSSは、シンプルな縮小のみを適用する
+     */
+    return {
+      map: { inline: false },
+      plugins: [
+        require('postcss-import'),
+        require('postcss-clean')({
+          level: {
+            1: {
+              all: false,
+              removeEmpty: true,
+              removeQuotes: true,
+              removeWhitespace: true,
+              selectorsSortingMethod: 'none',
+              tidyAtRules: true,
+              tidyBlockScopes: true,
+            },
+          },
+        }),
+      ],
+    };
+  } else {
+    /*
+     * そうでない場合は、強力な圧縮を実行
+     */
+    return {
+      map: { inline: false },
+      plugins: [
+        require('postcss-import'),
+        require('autoprefixer')({
+          remove: false,
+        }),
+        require('postcss-clean')({
+          level: 2,
+        }),
+      ],
+    };
+  }
 };
