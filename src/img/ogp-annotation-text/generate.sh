ec98b366adfc5a301598a7ec574d0a710b6e938e
Sonishi Izuka <sounisi5011@jedofuka.local>
2018-04-10 03:54:57 +0900

    Add annotation text image

---
+++ src/img/ogp-annotation-text/generate.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+convert -background none -fill white \
+  -font 'YuGothic-Bold' -pointsize 23 -gravity center \
+  label:'Art by MisOdeN\nSource: https://twitter.com/wodnuyRnaiR/status/977500688279154688' \
+  ./text.png
