d92ea3430c8546fa2e1fa6bb4a65523d6e0a3a63
Sonishi Izuka <sounisi5011@jedofuka.local>
2018-04-09 09:27:05 +0900

    Add urlToFilename function and print console funcs in image-download.js

--- bin/image-download.js
+++ bin/image-download.js
@@ -3,16 +3,38 @@ const path = require('path');
 const rp = require('request-promise-native');
 const mkdirp = require('mkdirp');
 
+const urlToFilename = urlStr => (
+  urlStr.replace(
+    /^https?:\/\/|[.\/:]/g,
+    char => (
+      char === '.' ? '{dot}' :
+      char === '/' ? '{slash}' :
+      char === ':' ? '{colon}' :
+      ''
+    )
+  )
+);
+
+const SOURCE_URI = 'https://twitter.com/wodnuyRnaiR/status/977500688279154688';
 const TARGET_URI = 'https://pbs.twimg.com/media/DZDHrURU0AEnlK8.jpg:orig';
 const SAVE_DIR = `${__dirname}/../cache`;
-const FILE_NAME = 'twitter{dot}com{slash}wodnuyRnaiR{slash}status{slash}977500688279154688.jpg';
+const FILE_NAME = urlToFilename(SOURCE_URI) + '.' + urlToFilename(TARGET_URI) + '.jpg';
 
+const filePath = path.join(SAVE_DIR, FILE_NAME);
+
+console.error(`Fetching image: ${TARGET_URI}`);
 rp({ uri: TARGET_URI, encoding: null })
   .then(body => {
+    console.error(`Fetch complete: ${TARGET_URI}`);
+    console.error(`Saving to ${FILE_NAME}`);
+
     mkdirp(SAVE_DIR, error => {
       if (!error) {
-        const FILE_PATH = path.join(SAVE_DIR, FILE_NAME);
-        fs.writeFile(FILE_PATH, body, error => {});
+        fs.writeFile(filePath, body, error => {
+          if (!error) {
+            console.error(`Saved to ${FILE_NAME}`);
+          }
+        });
       }
     });
   });
