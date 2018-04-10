4594dc802adecf101da51419e9fc48153dcf0d59
Sonishi Izuka <sounisi5011@jedofuka.local>
2018-04-10 15:22:15 +0900

    Edit ogp-image-generate.js: Added code to generate 1.91:1 ratio image

--- bin/ogp-image-generate.js
+++ bin/ogp-image-generate.js
@@ -29,7 +29,7 @@ urlFetch(TARGET_URI, { ext: 'jpg' })
   .then(([image, textImage]) => {
     const {width: originalWidth, height: originalHeight} = image.bitmap;
     const cropY = 35;
-    const bgcolor = 'black';
+    const bgcolor = 0x000000FF;
 
     /*
      * 必要な箇所をクロップする
@@ -46,9 +46,28 @@ urlFetch(TARGET_URI, { ext: 'jpg' })
       image.composite(textImage, xDiff, currentHeight - textHeight - xDiff);
     }
 
+    /**
+     * 1.91:1サイズの背景画像を生成
+     * @see https://github.com/oliver-moran/jimp/issues/167#issuecomment-249063832
+     * @see https://github.com/oliver-moran/jimp/tree/f7b5e5b543b012069c513ae8a2368c388e54e6ad#creating-new-images
+     */
+    const horizontalMargin = Math.round(((currentHeight * 1.91) - currentHeight) / 2);
+    const verticalMargin = 0;
+    const ogpSize = [
+      currentHeight + (horizontalMargin * 2),
+      currentHeight,
+    ];
+    const canvasImage = new Jimp(...ogpSize, bgcolor);
+
+    /*
+     * 背景画像に、対象の画像を合成
+     */
+    canvasImage.composite(image, horizontalMargin, verticalMargin);
+
     /*
      * 保存する
      */
     image.write(`${__dirname}/../cache/ogp-out-test.png`);
+    canvasImage.write(`${__dirname}/../cache/ogp-canvas-test.png`);
   })
   .catch(error => console.error(error));
