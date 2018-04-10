4914363ce5341e36b5a8002e7173f905e62afc8f
Sonishi Izuka <sounisi5011@jedofuka.local>
2018-04-10 14:44:51 +0900

    Edit ogp-image-generate.js: Change annotation text addition processing from insertion of characters to composition of images

--- bin/ogp-image-generate.js
+++ bin/ogp-image-generate.js
@@ -21,52 +21,12 @@ const urlToFilename = urlStr => (
 const SOURCE_URI = 'https://twitter.com/wodnuyRnaiR/status/977500688279154688';
 const TARGET_URI = 'https://pbs.twimg.com/media/DZDHrURU0AEnlK8.jpg:orig';
 
-/**
- * @see https://github.com/oliver-moran/jimp/blob/f207a81d34640fb56e2f76ae53a1a11b44455ea7/index.js#L2515-L2525
- */
-function measureText(font, text) {
-  var x = 0;
-  for (var i = 0; i < text.length; i++) {
-    if (font.chars[text[i]]) {
-      x += 0
-        //+ font.chars[text[i]].xoffset
-        + (font.kernings[text[i]] && font.kernings[text[i]][text[i+1]] ? font.kernings[text[i]][text[i+1]] : 0)
-        + (font.chars[text[i]].xadvance || 0);
-    }
-  }
-  return x;
-};
-
-/**
- * @see https://github.com/oliver-moran/jimp/blob/2d93d5dcdf78ba09f9c203f508e3c3ca9a036abc/index.js#L2747-L2765
- */
-function measureTextHeight(font, text, maxWidth = Infinity) {
-  var words = text.split(' ');
-  var line = '';
-  var textTotalHeight = font.common.lineHeight;
-
-  for (let n = 0; n < words.length; n++) {
-    let testLine = line + words[n] + ' ';
-    let testWidth = measureText(font, testLine);
-
-    if (testWidth > maxWidth && n > 0) {
-      textTotalHeight += font.common.lineHeight;
-      line = words[n] + ' ';
-    } else {
-      line = testLine;
-    }
-  }
-
-  return textTotalHeight;
-}
-
 urlFetch(TARGET_URI, { ext: 'jpg' })
   .then(filepath => Promise.all([
     Jimp.read(filepath),
-    Jimp.loadFont(Jimp.FONT_SANS_16_WHITE),
-    Jimp.loadFont(Jimp.FONT_SANS_32_WHITE),
+    Jimp.read(`${__dirname}/../src/img/ogp-annotation-text/text.png`),
   ]))
-  .then(([image, font16, font32]) => {
+  .then(([image, textImage]) => {
     const {width: originalWidth, height: originalHeight} = image.bitmap;
     const cropY = 35;
     const bgcolor = 'black';
@@ -80,24 +40,10 @@ urlFetch(TARGET_URI, { ext: 'jpg' })
     /*
      * 著作者情報のテキストを追加する
      */
-    let bottomPos = 0;
     {
-      const font = font16;
-      const text = 'Source: https://twitter.com/wodnuyRnaiR/status/977500688279154688';
-      const textWidth = measureText(font, text);
-      const textHeight = measureTextHeight(font, text);
-      const xPos = Math.floor((currentWidth - textWidth) / 2);
-      const yPos = currentHeight - (bottomPos += textHeight);
-      image.print(font, xPos, yPos, text);
-    }
-    {
-      const font = font32;
-      const text = 'Art by MisOdeN';
-      const textWidth = measureText(font, text);
-      const textHeight = measureTextHeight(font, text);
-      const xPos = Math.floor((currentWidth - textWidth) / 2);
-      const yPos = currentHeight - (bottomPos += textHeight);
-      image.print(font, xPos, yPos, text);
+      const {width: textWidth, height: textHeight} = textImage.bitmap;
+      const xDiff = (currentWidth - textWidth) / 2;
+      image.composite(textImage, xDiff, currentHeight - textHeight - xDiff);
     }
 
     /*
