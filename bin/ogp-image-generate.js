58686320652966daaa69e2a7235f7b703c7eb30a
Sonishi Izuka <sounisi5011@jedofuka.local>
2018-04-10 23:27:59 +0900

    Edit ogp-image-generate.js: Add jimpPngWrite function

--- bin/ogp-image-generate.js
+++ bin/ogp-image-generate.js
@@ -19,6 +19,76 @@ const urlToFilename = urlStr => (
   )
 );
 
+/**
+ * Jimpで、最も小さく圧縮されたPNG画像を書き出す関数
+ *
+ * @param {Jimp} image
+ * @param {string} filepath
+ * @return {Promise}
+ */
+const jimpPngWrite = (image, filepath) => new Promise((resolve, reject) => {
+  if (!(image instanceof Jimp)) {
+    reject(throw new TypeError('image parameter must be a Jimp object'));
+    return;
+  }
+
+  /*
+   * 副作用を防ぐため、Jimpオブジェクトをコピーする
+   * TODO: 関数実行時にコピーするかどうかを選択できるようにするオプションの追加。
+   */
+  const targetImage = image.clone();
+
+  /*
+   * 最低限必要なパラメータを定義
+   * TODO: 透過PNGの場合は、rgbaはtrueにしておくコード、またはオプション。
+   */
+  targetImage.rgba(false);
+  targetImage.deflateLevel(9);
+
+  /*
+   * 様々なパラメータ設定でPNG画像データを生成する
+   */
+  const waitPromiseList = [];
+  let imageBuffer = null;
+  for (const filterType of ['AUTO', 'NONE', 'SUB', 'UP', 'AVERAGE', 'PAETH']) {
+    targetImage.filterType(Jimp[`PNG_FILTER_${filterType}`]);
+
+    for (const deflateStrategy of [0, 1, 2, 3]) {
+      targetImage.deflateStrategy(deflateStrategy);
+
+      waitPromiseList.push(new Promise(resolve => {
+        targetImage.getBuffer(Jimp.MIME_PNG, (err, buffer) => {
+          if (!err) {
+            const prevBuf = imageBuffer;
+            if (!prevBuf || buffer.length < prevBuf.length) {
+              imageBuffer = buffer;
+            }
+          }
+          // TODO: エラーだけでなく、この時のパラメータ設定も返す
+          resolve(err);
+        });
+      }));
+    }
+  }
+
+  /*
+   * PNG画像データが生成されるまで待機する
+   */
+  Promise.all(waitPromiseList).then(valueList => {
+    if (!imageBuffer) {
+      reject(new Error('PNG image data could not be generated'));
+      return;
+    }
+    fs.writeFile(filepath, imageBuffer, err => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve();
+      }
+    });
+  });
+});
+
 const SOURCE_URI = 'https://twitter.com/wodnuyRnaiR/status/977500688279154688';
 const TARGET_URI = 'https://pbs.twimg.com/media/DZDHrURU0AEnlK8.jpg:orig';
 
