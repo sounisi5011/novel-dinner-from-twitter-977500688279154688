527b82083aebefeb9d66721c9453aa1db113c514
Sonishi Izuka <sounisi5011@jedofuka.local>
2018-04-12 04:05:22 +0900

    Include fs modules in bin/social-icons-download.js

--- bin/social-icons-download.js
+++ bin/social-icons-download.js
@@ -2,8 +2,8 @@
  * ソーシャルボタン用の画像ファイルを自動で取得する
  */
 
+const fs = require('fs');
 const path = require('path');
-const cpy = require('cpy');
 const urlFetch = require('../lib/urlFetch');
 
 const urlToFilename = urlStr => (
