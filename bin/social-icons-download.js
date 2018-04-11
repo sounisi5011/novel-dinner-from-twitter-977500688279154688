35b5bdf1446d5555d1ac49b4c094c20c01a0fb88
Sonishi Izuka <sounisi5011@jedofuka.local>
2018-04-12 03:33:03 +0900

    Define URL_LIST constant in bin/social-icons-download.js

--- bin/social-icons-download.js
+++ bin/social-icons-download.js
@@ -5,3 +5,17 @@
 const fs = require('fs');
 const path = require('path');
 const urlFetch = require('../lib/urlFetch');
+
+const URL_LIST = [
+  /*
+   * https://developers.google.com/+/web/share/?hl=ja#sharelink-sizes
+   */
+  'https://www.gstatic.com/images/icons/gplus-64.png',
+  'https://www.gstatic.com/images/icons/gplus-32.png',
+  'https://www.gstatic.com/images/icons/gplus-16.png',
+
+  /*
+   * http://b.hatena.ne.jp/guide/bbutton
+   */
+  'https://b.st-hatena.com/images/entry-button/button-only@2x.png',
+];
