b087f7e948b80c861ca15c79efbe3a22098310b2
Sonishi Izuka <sounisi5011@jedofuka.local>
2018-04-17 19:56:16 +0900

    Added ESLint setting to .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,4 +3,20 @@
 const OFF = 0;
 const ERROR = 2;
 
-module.exports = {};
+module.exports = {
+  env: {
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        'singleQuote': true,
+      },
+    ],
+  },
+};
